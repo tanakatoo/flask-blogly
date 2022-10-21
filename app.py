0e0a1d39c85ab70310395ab91db83eaf43c8879f
Karmen Too <karmen.tanaka@gmail.com>
2022-10-21 15:33:39 -0400

    not working

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 """Blogly application."""
+from flask import Flask, request, jsonify, redirect, render_template, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
-from flask import Flask
-from models import db, connect_db
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'ohsosecret'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS']= False
+debug=DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
--- models.py
+++ models.py
@@ -1,1 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db=SQLAlchemy()
+def connect_db(app):
+    db.app=app
+    db.init_app(app)
+    app.app_context().push()
+
+
+
 """Models for Blogly."""
+
+class User(db.Model):
+    """User Model"""
+    __tablename__="User"
+    def __repr__(self):
+        return f""
+    
+    id=db.column(db.Integer,
+                 primary_key=True,
+                autoincrement=True)
+    first_name=db.column(db.String(50), 
+                         nullable=False)
+    last_name=db.column(db.String(50))
+    image_url=db.column(db.LargeBinary)
+    
+    
+    
+    
