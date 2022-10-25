a240e4a2a6d8f0e9fb030c9f6d6e312e5acaa5b3
Karmen Too <karmen.tanaka@gmail.com>
2022-10-25 14:26:08 -0400

    done

--- tests/test_route.py
+++ tests/test_route.py
@@ -20,20 +20,44 @@ class TestRoutes(TestCase):
         db.session.add(user)
         db.session.commit()
     
-    # @classmethod
-    # def tearDownClass(cls):
-        # db.session.rollback()
+    @classmethod
+    def tearDownClass(cls):
+        db.session.rollback()
         
     def test_new(self):
         with app.test_client() as client:
-            res=client.post('/users/new', data={'firstName':'John','lastName':'Doe', 'imageURL':'https://eloquentjavascript.net/img/chapter_picture_6.jpg'})
+            res=client.post('/users/new', data={'firstName':'John','lastName':'Doe', 'imageURL':'https://eloquentjavascript.net/img/chapter_picture_6.jpg'}, follow_redirects=True)
             html=res.get_data(as_text=True)
             
-            self.assertIn('Karmen', html)
+            self.assertEqual(res.status_code,200)
+            self.assertIn('John', html)
             self.assertIn('Doe', html)
     
     def test_edit(self):
         with app.test_client() as client:
-            res=client.post('/users/1/', data={'firstName':'Karmen The Second','lastName':'Too', 'imageURL':'https://eloquentjavascript.net/img/chapter_picture_6.jpg'})
+            res=client.post('/users/1/edit', data={'firstName':'Karmen The Second','lastName':'Too', 'imageURL':'https://eloquentjavascript.net/img/chapter_picture_6.jpg'}, follow_redirects=True)
             html=res.get_data(as_text=True)
             
+            self.assertEqual(res.status_code,200)
+            self.assertIn('Karmen The Second',html)
+    
+    def test_allListings(self):
+        user=User(first_name="Jane",last_name="Two",image_url="https://eloquentjavascript.net/img/chapter_picture_6.jpg")
+        db.session.add(user)
+        db.session.commit()
+
+        with app.test_client() as client:
+            res=client.get('/users')
+            html=res.get_data(as_text=True)
+            
+            self.assertEqual(res.status_code,200)
+            self.assertIn('Jane', html)
+            self.assertIn('Karmen', html)
+            
+    def test_delete(self):
+        with app.test_client() as client:
+            res=client.post('/users/1/delete', follow_redirects=True)
+            html=res.get_data(as_text=True)
+            
+            self.assertEqual(res.status_code,200)
+            self.assertNotIn('Karmen', html)
